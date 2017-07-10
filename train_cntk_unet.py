fe0352ca216f117e343abe1d59d0b00abf2446c2
tebesk <lord.of.the.red@gmail.com>
2017-07-10 18:50:55 +0900

    batch modify

--- train_cntk_unet.py
+++ train_cntk_unet.py
@@ -6,19 +6,33 @@ from cntk.learners import sgd, learning_rate_schedule, UnitType
 from cntk.device import gpu, try_set_default_device
 from PIL import Image
 from PIL import ImageOps
-import cntk_unet
+import cntk_unet512 as cntk_unet
 import time
 try_set_default_device(gpu(0))
 print ("-------------------------")
-def train(AnsPath, BasePath, list, use_existing=False):
-    testimg= Image.open(AnsPath+"/"+list[0])
-    testimg = testimg.resize((256,512))
-    testimg = ImageOps.grayscale(testimg)
 
-    test_image = np.array(testimg)#.transpose(2,0,1)
-    test_image = np.array([test_image])
-    shape = test_image.shape
-    data_size = test_image.shape[0]
+def Img2CntkImg(path, resizeX, resizeY):
+    img = Image.open(path)
+    img = img.resize((256, 512))
+    img = ImageOps.grayscale(img)
+
+
+    training_img = np.array(img)
+    training_img = np.array([training_img])
+    training_img = training_img.astype(np.float32)
+
+    del img
+    return training_img
+
+def train(AnsPath, TrnPath, list, use_existing=False):
+
+    # Get minibatches of training data and perform model training
+    minibatch_size = 10
+    num_epochs = 1000
+    test = np.zeros((1, 512, 256))
+    shape = test.shape
+    print("test image shape:"+str(test.shape))
+
 
     x = C.input_variable(shape)
     y = C.input_variable(shape)
@@ -35,37 +49,55 @@ def train(AnsPath, BasePath, list, use_existing=False):
     momentum = C.learners.momentum_as_time_constant_schedule(0)
     trainer = C.Trainer(z, (-dice_coef, -dice_coef), C.learners.adam(z.parameters, lr=lr, momentum=momentum))
 
-    # Get minibatches of training data and perform model training
-    minibatch_size = 20
-    num_epochs = 2000
-    num_mb_per_epoch = int(data_size / minibatch_size)
+    file_num = len(list)
+
+    training_errors = []
+    test_errors = []
+
+
+
 
     sw = time.time()
     for e in range(0, num_epochs):
-        for i in range(0, num_mb_per_epoch):
-            img_y = Image.open(AnsPath+"/"+list[i * minibatch_size:(i + 1) * minibatch_size])
-            img_x = Image.open(BasePath+"/"+list[i * minibatch_size:(i + 1) * minibatch_size])
-            img_x = img_x.resize((256,512))
-            img_y = img_y.resize((256,512))
-            img_y = ImageOps.grayscale(img_y)
-            img_x = ImageOps.grayscale(img_x)
 
-            training_y = (np.array(img_y))#.transpose(2,0,1)
-            training_x = (np.array(img_x))#.transpose(2,0,1)
-            training_y = (np.array(training_y))
-            training_x = (np.array(training_x))
+        num =0
+        for i in range(0, file_num):
+            #print(i%minibatch_size)
+            if i%minibatch_size ==0:
+                training_y = np.array([Img2CntkImg(AnsPath + "/" + list[i], 256, 512)])
+                training_x = np.array([Img2CntkImg(TrnPath + "/" + list[i], 256, 512)])
+                #print("[1]"+str(training_y.shape))
+            elif i%minibatch_size >0 and i%minibatch_size<minibatch_size-1:
+                training_y = np.append(training_y,np.array([Img2CntkImg(AnsPath+"/"+list[i], 256, 512)]),axis=0)
+                training_x = np.append(training_x,np.array([Img2CntkImg(TrnPath+"/"+list[i], 256, 512)]),axis=0)
+                #print("[2]"+str(training_y.shape))
+            elif i%minibatch_size==minibatch_size-1:
+                training_y = np.append(training_y,np.array([Img2CntkImg(AnsPath+"/"+list[i], 256, 512)]),axis=0)
+                training_x = np.append(training_x,np.array([Img2CntkImg(TrnPath+"/"+list[i], 256, 512)]),axis=0)
+                trainer.train_minibatch({x: training_x, y: training_y})
+                #print("[3]"+str(training_y.shape))
+                #print("###################")
+                if i == num_epochs-1:
+                    # Measure training error
+                    training_errors.append(trainer.test_minibatch({x: training_x, y: training_y}))
+                    print("Epoch:" + str(e) + "  Error:" + str(np.mean(training_errors)) + "  time:" + str(time.time() - sw))
 
-            trainer.train_minibatch({x: training_x, y: training_y})
+        #print("epoch:" + str(e) + "  error:"+str(np.mean(training_errors))+"  time:" + str(time.time()-sw))
+        training_errors = []
 
-        if num_epochs%500==0:
-            trainer.save_checkpoint("/home/ys/PycharmProjects/cntk-unet/cntk-unet"+str(e)+".dnn")
+        if e%50==0:
+            trainer.save_checkpoint("/home/ys/PycharmProjects/cntk-unet/batch15_cntk-unet512_Zraw_"+str(e)+".dnn")
             print ("epoch:"+str(e))
             print ("time passed:"+str(time.time()-sw))
 
     return trainer
 
 if __name__ == '__main__':
-    AnsPath  = "/home/ys/Share/7_DL_model_set/ver20170413/15Z32/DL_Ans_half"
-    BasePath = "/home/ys/Share/7_DL_model_set/ver20170413/15Z32/trn_half"
+    AnsPath  = "/home/ys/Share/7_DL_model_set/ver20170413/Z_White"
+    BasePath = "/home/ys/Share/7_DL_model_set/ver20170413/Z_raw"
     filelist = os.listdir(AnsPath)
-    train(AnsPath, BasePath, filelist, False)
+    trainer, training_errors, test_errors =  train(AnsPath, BasePath, filelist, False)
+    print (trainer)
+    print (training_errors)
+    print (test_errors)
+
